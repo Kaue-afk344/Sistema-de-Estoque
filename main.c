3a191c77e13fb651c79cfbd3ff557644642ccd89
J Henrique <henriqueds2024@hotmail.com>
2026-09-19 23:29:04 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+
+
+////                    seção produtos                           ////
+typedef struct
+{
+    char nome[MAX];
+    char codigo[MAX];
+    char categoria[MAX];
+    int qtd_estoque;
+    float preco;
+    char tamanho[MAX];
+} Produto;
+
+void alloca_produtos(){
+
+//allocamos a variavel produtos zerada e crescemos conforme cadastramos
+Produto *produtos = NULL;
+//esta variavel se refere a quantidade de produtos no vetor de produtos
+int quantidade = 0;
+}
+
+void menu_produtos(){
+int escolha = 0;
+    printf("=====MENU PRODUTOS=====\n ");
+        printf("1 - Cadastrar Produto\n ");
+        printf("2 - Buscar Produtos\n ");
+        printf("3 - Exibir estoque\n ");
+        printf("4 - Editar Produtos\n");
+        printf("5 - Sair\n ");
+        scanf("%i", &escolha);
+}
+////                    seção produtos                           ////
+
+
+
+int main(){
+int escolha = 0;
+    do{
+        menu_produtos();
+    } while (escolha > 0);
+
+    return 0;
+}
