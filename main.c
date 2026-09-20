bb6713573ec7bea4220c02e2ca2f3423ce6ee5db
J Henrique <henriqueds2024@hotmail.com>
2026-09-20 11:17:05 -0300

    update main

--- main.c
+++ main.c
@@ -16,13 +16,11 @@ typedef struct
     char tamanho[MAX];
 } Produto;
 
-void alloca_produtos(){
-
 //allocamos a variavel produtos zerada e crescemos conforme cadastramos
 Produto *produtos = NULL;
 //esta variavel se refere a quantidade de produtos no vetor de produtos
 int quantidade = 0;
-}
+
 void buscar_codigo(Produto produtos[],int quantidade){ //função buscar codigo| procura produtos e quantidade na struct Produto
     char codigo_busca[MAX]; //variavel que guarda o codigo que o usuario quer buscar
     printf("Digite o codigo do produto do produto que deseja buscar: ");
@@ -49,26 +47,45 @@ void buscar_codigo(Produto produtos[],int quantidade){ //função buscar codigo|
         }
 }
 
-void menu_produtos(){
-int escolha = 0;
-    printf("=====MENU PRODUTOS=====\n ");
+int menu_produtos(){
+    int escolha;
+    printf("\n=====MENU PRODUTOS=====\n ");
         printf("1 - Cadastrar Produto\n ");
         printf("2 - Buscar Produtos\n ");
         printf("3 - Exibir Estoque\n ");
-        printf("4 - Editar Produtos\n");
-        printf("5 - Alterar Estoque\n");
+        printf("4 - Editar Produtos\n ");
+        printf("5 - Alterar Estoque\n ");
         printf("0 - Sair\n ");
+        printf("Digite uma opcao: ");
         scanf("%i", &escolha);
+    return escolha;
 }
 ////                    seção produtos                           ////
 
 
 
 int main(){
-int escolha = 0;
+int opcao;
     do{
-        menu_produtos();
-    } while (escolha > 0);
-
+        opcao = menu_produtos();
+        switch (opcao){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 0:
+                break;
+            default:
+                printf("Opcao invalida\n");
+                break;
+        }
+    } while (opcao!=0);
+    
     return 0;
 }
