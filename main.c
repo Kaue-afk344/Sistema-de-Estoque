511e3cddfd379a410630ec8c26194a6cc77cc7cf
Kaio <kaiocesar890@gmail.com>
2026-09-20 15:06:37 -0300

    Atualização do Código
    Adicionei:
    - limpa buffer
    - reallocar produtos
    - verificar codigo
    - cadastro codigo
    - verificar nome
    - cadastro nome
    
    Caso necessario, podem utilizar de algumas funções auxiliares em outras partes do codigo, como a função de limpar buffer

--- main.c
+++ main.c
@@ -21,6 +21,82 @@ Produto *produtos = NULL;
 //esta variavel se refere a quantidade de produtos no vetor de produtos
 int quantidade = 0;
 
+void limpar_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void reallocar_produtos(){ //função para realocar produtos
+    quantidade++;
+    produtos = realloc(produtos, quantidade * sizeof(Produto)); //realocando produtos com a função realloc
+}
+
+int verificar_codigo(char codigo_digitado[], Produto produtos[], int quantidade) {
+    for (int i = 0; i < quantidade-1; i++) {
+        if (strcmp(produtos[i].codigo, codigo_digitado) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+void cadastro_codigo(){
+    do{
+        char codigo_digitado[MAX];
+        scanf(" %99[^\n]",codigo_digitado);
+        if((verificar_codigo(codigo_digitado, produtos, quantidade))==0){
+            strcpy(produtos[quantidade - 1].codigo, codigo_digitado);
+            break;
+        }else{
+            printf("Codigo ja registrado, tente outro!\n"); 
+        }
+    }while(1);
+}
+
+
+
+int verificar_nome(char nome_digitado[], Produto produtos[], int quantidade) {
+    for (int i = 0; i < quantidade-1; i++) {
+        if (strcmp(produtos[i].nome, nome_digitado) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void cadastro_nome(){
+    do{
+        char nome_digitado[MAX];
+        scanf(" %99[^\n]",nome_digitado);
+        if((verificar_nome(nome_digitado, produtos, quantidade))==0){
+            strcpy(produtos[quantidade - 1].nome, nome_digitado);
+            break;
+        }else{
+            printf("Nome ja registrado, tente outro!\n"); 
+        }
+    }while(1);
+}
+
+
+void cadastrar_produto(){ //função cadastrar produto| procura produtos e quantidade na struct Produto
+    reallocar_produtos();
+    int i = quantidade - 1;
+    
+    printf("Digite o nome do produto: ");
+    cadastro_nome();
+    printf("Digite o codigo do produto: ");
+    cadastro_codigo();
+    printf("Digite a categoria do produto: ");
+    scanf(" %99[^\n]",produtos[i].categoria);
+    printf("Digite a quantidade em estoque do produto: ");
+    scanf("%d",&produtos[i].qtd_estoque);
+    printf("Digite o preco do produto: ");
+    scanf("%f",&produtos[i].preco);
+    limpar_buffer();
+    printf("Digite o tamanho do produto: ");
+    scanf("%s",produtos[i].tamanho);
+
+}
+
 void exibirInfoProduto(Produto produtos[], int i){
     printf("PRODUTO %i",i+1 );
     printf("%s",produtos[i].nome);
@@ -31,48 +107,32 @@ void exibirInfoProduto(Produto produtos[], int i){
     printf("Preco: %.2f\n", produtos[i].preco);
 } // funcao que sera usada para exibir as informacoes dos produtos tanto na funcao exibir_estoque quanto na buscar_produto
 
-//pedro | procurar codigo
+
 void buscar_codigo(Produto produtos[],int quantidade){ //função buscar codigo| procura produtos e quantidade na struct Produto
-    if(quantidade==0){
-        printf("Nenhum produto cadastrado.");
-        return;
-    }
     char codigo_busca[MAX]; //variavel que guarda o codigo que o usuario quer buscar
     printf("Digite o codigo do produto do produto que deseja buscar: ");
-    scanf("%s",codigo_busca); //guarda o input do usuario na var codigo_busca
-    int encontrado = 0;// variavel que verifica se o produto foi encontrado
+    scanf("%s",codigo_busca);
+    int encontrado = 0; // variavel que verifica se o produto foi
+
     for(int i =0;i<quantidade;i++){// for para procurar na var quantidade
         if (strcmp(produtos[i].codigo,codigo_busca)==0){ //função da biblioteca string para comparar strings| compara produtos e codigo_busca
-            exibirInfoProduto(produtos, i);// chama a função exibir info produto
-            encontrado=1; // se encontrado for 1, faz o laço parar
+            printf("\n--- Produto encontrado ---\n"); // print para mostras as informações da busca
+            printf("Nome: %s",produtos[i].nome); // print para mostras as informações da busca
+            printf("Codigo:  %s",produtos[i].codigo); // print para mostras as informações da busca 
+            printf("Categoria %s",produtos[i].categoria); // print para mostras as informações da busca 
+            printf("Quantidade em estoque %d",produtos[i].qtd_estoque); // print para mostras as informações da busca
+            printf("Preco: %.2f",produtos[i].preco); // print para mostras as informações da busca
+
+            encontrado=1; // se encontrado for 1, para o laço
             break;
+
         }
+        
     }
     if(encontrado==0){//se for rodar e não achar nada, mostrar no print que não achou nenhum produto
             printf("Produto não encontrado.");
         }
-    }//pedro|procurar codigo
-
-    //pedro|buscar por nome
-    void buscar_nome(Produto produtos[],int quantidade){
-        if(quantidade==0){
-            printf("Nenhum produto cadastrado.");
-            return;
-        }
-        char nome_busca[MAX]; // variavel que guarda o nome que o usuario quer buscar
-        printf("Digite o nome do produto que deseja buscar.");
-        scanf(" %[^\n]",nome_busca); //guarda o input do usuario na var nome_busca
-        int encontrado =0; // variavel que verifica se o produto foi encontrado
-        for(int i=0;i<quantidade;i++){
-            if(strcmp(produtos[i].nome,nome_busca)==0){
-                 exibirInfoProduto(produtos, i);// chama a função exibir info produto
-                 encontrado=1;// se encontrado for 1, faz o laço para
-                 }
-            }
-            if(encontrado==0){
-                printf("Produto nao encontrado.");
-            }
-        }//pedro buscar por nome
+}
 
 void exibir_estoque(Produto produtos[],int quantidade){
     if(quantidade==0){
@@ -112,6 +172,7 @@ int menu_produtos(){
         printf("0 - Sair\n ");
         printf("Digite uma opcao: ");
         scanf("%i", &escolha);
+        limpar_buffer();
     return escolha;
 } // funcao que exibe o menu do sistema de estoque e retorna o inteiro digitado (escolha) pelo usuario
 
@@ -125,24 +186,10 @@ int opcao;
         opcao = menu_produtos();
         switch (opcao){
             case 1:
+                cadastrar_produto();
                 break;
             case 2:
-            //pedro|case
-            int opc_busca;//var para guardar a opcao que o usuario digitou
-            printf("=====Buscar produtos=====");
-            printf("1 - Buscar por codigo.");
-            printf("2 - Buscar por nome.");
-            print("Digite uma opcao: ");
-            scanf("d",&opc_busca);
-
-            if(opc_busca ==1){ //se o usuario escolher 1, chama a função buscar por codigo
-                busca_codigo(produtos,quantidade);
-            }else if(opc_busca ==2){ // se o usuario escolher 2, chama a funcao buscar por nome
-                busca_nome(produtos,quantidade);
-            }else{// se ele nao digitar nada, diz que nenhuma opcao foi encontrada
-                printf("Opcao nao encontrada.");
-            }
-                break;//pedro|case2
+                break;
             case 3:
                 break;
             case 4:
