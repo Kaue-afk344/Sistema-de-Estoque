b3bef443d5046c97c50e46b1b94ff281107e1125
Pedro Victor Façanha dos Santos <pfacanha4@gmail.com>
2026-09-20 10:57:42 -0300

    Implementa funcao buscar_por_codigo

--- main.c
+++ main.c
@@ -23,6 +23,31 @@ Produto *produtos = NULL;
 //esta variavel se refere a quantidade de produtos no vetor de produtos
 int quantidade = 0;
 }
+void buscar_codigo(Produto produtos[],int quantidade){ //função buscar codigo| procura produtos e quantidade na struct Produto
+    char codigo_busca[MAX]; //variavel que guarda o codigo que o usuario quer buscar
+    printf("Digite o codigo do produto do produto que deseja buscar: ");
+    scanf("%s",codigo_busca);
+    int encontrado = 0; // variavel que verifica se o produto foi
+
+    for(int i =0;i<quantidade;i++){// for para procurar na var quantidade
+        if (strcmp(produtos[i].codigo,codigo_busca)==0){ //função da biblioteca string para comparar strings| compara produtos e codigo_busca
+            printf("\n--- Produto encontrado ---\n"); // print para mostras as informações da busca
+            printf("Nome: %s",produtos[i].nome); // print para mostras as informações da busca
+            printf("Codigo:  %s",produtos[i].codigo); // print para mostras as informações da busca 
+            printf("Categoria %s",produtos[i].categoria); // print para mostras as informações da busca 
+            printf("Quantidade em estoque %d",produtos[i].qtd_estoque); // print para mostras as informações da busca
+            printf("Preco: %.2f",produtos[i].preco); // print para mostras as informações da busca
+
+            encontrado=1; // se encontrado for 1, para o laço
+            break;
+
+        }
+        
+    }
+    if(encontrado==0){//se for rodar e não achar nada, mostrar no print que não achou nenhum produto
+            printf("Produto não encontrado.");
+        }
+}
 
 void menu_produtos(){
 int escolha = 0;
